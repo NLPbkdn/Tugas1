6ecd912d4b96307fe373d272f96d3edc5463cae2
SatriaPriambada <satriapriambada@gmail.com>
2016-09-17 23:54:37 +0700

    adding stringtowordvector

--- SpamDetector/src/spamdetector/SpamDetector.java
+++ SpamDetector/src/spamdetector/SpamDetector.java
@@ -12,10 +12,6 @@ import java.io.IOException;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.evaluation.NominalPrediction;
-import weka.classifiers.rules.DecisionTable;
-import weka.classifiers.rules.PART;
-import weka.classifiers.trees.DecisionStump;
-import weka.classifiers.trees.J48;
 import weka.core.FastVector;
 import weka.core.Instances;
 
